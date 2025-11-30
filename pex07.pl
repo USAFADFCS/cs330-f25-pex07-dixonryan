268c8aba403c61bdd75f61ad37cf69f1df5a11cb
dixonryan <c27dixon.ryan@afacademy.af.edu>
2025-11-30 16:18:07 -0700

    Donezo

--- pex07.pl
+++ pex07.pl
@@ -3,67 +3,78 @@
 %
 % name: Dixon Ryan
 %
-% Documentation: Used HW07 code -> most of inspiration
+% Documentation: Used HW07 code -> most of inspiration;
+$                Chat had to help me install proLog again (IT WROTE NO CODE FOR ME)
 %
 
 
-C4C(Chen).
-C4C(Garcia).
-C4C(Smith).
-C4C(Jones).
+c4C(chen).
+c4C(garcia).
+c4C(smith).
+c4C(jones).
 
-Sighting(WeatherBalloon).
-Sighting(Kite).
-Sighting(Fighter).
-Sighting(Cloud).
+sighting(weatherBalloon).
+sighting(kite).
+sighting(fighter).
+sighting(cloud).
 
-Day(Tuesday).
-Day(Wednesday).
-Day(Thursday).
-Day(Friday).
+day(tuesday).
+day(wednesday).
+day(thursday).
+day(friday).
 
 solve :-
-    Sighting(ChenSighting), Sighting(GarciaSighting), Sighting(SmithSighting), Sighting(JonesSighting),
+    sighting(ChenSighting), sighting(GarciaSighting), sighting(SmithSighting), sighting(JonesSighting),
     all_different([ChenSighting, GarciaSighting, SmithSighting, JonesSighting]),
 
-    Day(ChenDay), Day(GarciaDay), Day(SmithDay), Day(JonesDay),
+    day(ChenDay), day(GarciaDay), day(SmithDay), day(JonesDay),
     all_different([ChenDay, GarciaDay, SmithDay, JonesDay]),
 
-    Triples = [[Chen, ChenSighting, ChenDay],
-               [Garcia, GarciaSighting, GarciaDay],
-               [Smith, SmithSighting, SmithDay],
-               [Jones, JonesSighting, JonesDay]],
+    Triples = [[chen, ChenSighting, ChenDay],
+               [garcia, GarciaSighting, GarciaDay],
+               [smith, SmithSighting, SmithDay],
+               [jones, JonesSighting, JonesDay]],
 
     % 1. Smith did not see weather balloon or kite
-    \+ member([Smith, WeatherBalloon, _], Triples),
-    \+ member([Smith, Kite, _], Triples),
+    \+ member([smith, weatherBalloon, _], Triples),
+    \+ member([smith, kite, _], Triples),
 
     % 2. C4C Garcia didnt see the kite
-    \+ member([Garcia, Kite, _], Triples),
+    \+ member([garcia, kite, _], Triples),
 
     % 3. Friday sighting by Chen or fighter viewer
-    (member([Chen, _, Friday], Triples); member([_,Fighter,Friday], Triples)),
+    (member([chen, _, friday], Triples); member([_,fighter,friday], Triples)),
 
     % 4. Kite not sighted on a Tuesday
-    \+ member([_, Kite, Tuesday], Triples),
+    \+ member([_, kite, tuesday], Triples),
 
     % 5. Jones and Garcia didnt see balloon
-    \+ member([Jones, WeatherBalloon, _], Triples),
-    \+ member([Garcia, WeatherBalloon, _], Triples),
+    \+ member([jones, weatherBalloon, _], Triples),
+    \+ member([garcia, weatherBalloon, _], Triples),
 
     % 6. Jones sighting not on Tuesday
-    \+ member([Jones, _, Tuesday], Triples),
+    \+ member([jones, _, tuesday], Triples),
 
     % 7. Smith saw Cloud
-    member([Smith, Cloud, _], Triples),
+    member([smith, cloud, _], Triples),
 
     % 8. Fighter aircraft spotted Friday
-    member([_, Fighter, Friday], Triples),
+    member([_, fighter, friday], Triples),
 
     % 9. Balloon not spotted Wednesday
-    \+ member([_, WeatherBalloon, Wednesday], Triples).
+    \+ member([_, weatherBalloon, wednesday], Triples),
 
+    member([TueName, TueSight, tuesday], Triples),
+    tell(TueName, tuesday, TueSight),
+    
+    member([WedName, WedSight, wednesday], Triples),
+    tell(WedName, wednesday, WedSight),
 
+    member([ThurName, ThurSight, thursday], Triples),
+    tell(ThurName, thursday, ThurSight),
+
+    member([FriName, FriSight, friday], Triples),
+    tell(FriName, friday, FriSight).
 
 all_different([H | T]) :- member(H, T), !, fail.
 all_different([_ | T]) :- all_different(T).
@@ -71,8 +82,8 @@ all_different([_]).
 
 
 tell(X, Y, Z) :-
-    write('Cadet '), write(X), write(' on '), write(Y),
-    write(' saw a '), write(Z), write('.'), nl.
+    write('Cadet '), write(X), write(' on '), write(Z),
+    write(' saw a '), write(Y), write('.'), nl.
 
 
 % The query to get the answer(s) or that there is no answer
